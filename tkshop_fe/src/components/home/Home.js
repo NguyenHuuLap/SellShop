5de9220e5f2e0b554dbb4f95e0f38e826f7a6f3a
Thiện Tuyến <nguyenthientuyen22702@gmail.com>
2023-09-02 23:25:26 +0700

    update FE

--- tkshop_fe/src/components/home/Home.js
+++ tkshop_fe/src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import {fc_increment,fc_decrement} from '../actions'
+import {fc_increment,fc_decrement} from '../../actions'
 export class Home extends Component {
     constructor(props) {
         super(props)
