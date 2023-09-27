6ed995265d2c33241a9946ae64ffc87c315d07a5
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2023-09-27 22:51:46 +0700

    Revert "first"
    This reverts commit 9492a76954d6168c9f25a47d7038a10449a920af.

--- tkshop_fe/src/App.js
+++ tkshop_fe/src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header.js';
-import Home from './components/home/Home.js';
-import { ThemeProvider, createTheme } from '@mui/material';
-
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  }
-});
-
+import Header from './components/header/header';
+import Home from './components/home/home';
+ 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
-
-    
-
+    <div className="App">
+        <Header />
+        <Home />
+    </div>
   );
 }
-
+ 
 export default App;
