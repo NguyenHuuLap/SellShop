9492a76954d6168c9f25a47d7038a10449a920af
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2023-09-27 22:51:46 +0700

    first

--- tkshop_fe/src/App.js
+++ tkshop_fe/src/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/header';
-import Home from './components/home/home';
- 
+import Header from './components/header/Header.js';
+import Home from './components/home/Home.js';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  }
+});
+
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Home />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+
+    
+
   );
 }
- 
+
 export default App;
---
+++ tkshop_fe/src/components/home/AdsFullWidth.js
@@ -0,0 +1,27 @@
+import { Container, Card, CardMedia } from '@mui/material';
+import React from 'react';
+
+
+const AdsFullWidth = () => {
+    return (
+        <>
+
+            <Card
+                sx={{
+                    width: '100%',
+                    margin: 'auto',
+                    boxShadow: 5
+                }}
+            >
+                <CardMedia
+                    component='img'
+                    sx={{ width: "100%"}}
+                    image="https://cdn2.cellphones.com.vn/insecure/rs:fill:1200:75/q:80/plain/https://dashboard.cellphones.com.vn/storage/banner-special-desk-b2s-new.png"
+                />
+            </Card>
+
+        </>
+    );
+}
+
+export default AdsFullWidth;
